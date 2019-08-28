f00267c1a97421f9ecca7c515c8cc441ef762e61
yuexiaobai <yuejiande@163.com>
2019-08-28 20:18:17 +0800

    LeetCode109 Convert Sorted List to Binary Search Tree

---
+++ src/LeetCode109.java
@@ -0,0 +1,75 @@
+/**
+ * 109. Convert Sorted List to Binary Search Tree
+ * https://leetcode.com/problems/convert-sorted-list-to-binary-search-tree/
+ * 参照108题，把链表中数据拷贝到list中可以解决此题，但是不是最佳方法
+ * 最有算法很难想到，模拟的中序遍历，参考leetcode官方题解
+ * https://leetcode-cn.com/problems/convert-sorted-list-to-binary-search-tree/solution/you-xu-lian-biao-zhuan-huan-er-cha-sou-suo-shu-by-/
+ * TODO 这个也需要复习
+ */
+public class LeetCode109 {
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+
+    private ListNode head;
+
+    private int findSize(ListNode head) {
+        ListNode ptr = head;
+        int c = 0;
+        while (ptr != null) {
+            ptr = ptr.next;
+            c += 1;
+        }
+        return c;
+    }
+
+    private TreeNode convertListToBST(int l, int r) {
+        // Invalid case
+        if (l > r) {
+            return null;
+        }
+
+        int mid = (l + r) / 2;
+
+        // First step of simulated inorder traversal. Recursively form
+        // the left half
+        TreeNode left = this.convertListToBST(l, mid - 1);
+
+        // Once left half is traversed, process the current node
+        TreeNode node = new TreeNode(this.head.val);
+        node.left = left;
+
+        // Maintain the invariance mentioned in the algorithm
+        this.head = this.head.next;
+
+        // Recurse on the right hand side and form BST out of them
+        node.right = this.convertListToBST(mid + 1, r);
+        return node;
+    }
+
+    public TreeNode sortedListToBST(ListNode head) {
+        // Get the size of the linked list first
+        int size = this.findSize(head);
+
+        this.head = head;
+
+        // Form the BST now that we know the size
+        return convertListToBST(0, size - 1);
+    }
+
+}
